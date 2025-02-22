5036dc9294427f85d10e6d38b5ec3766edf69c70
Lucas Camargo <lb.camargo94@gmail.com>
2025-02-22 13:33:04 -0300

    feat(): The first hello world!

---
+++ vue-project/src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <H1>
+    Hello World!!
+  </H1>
+</template>
+
+<style scoped>
+h1 {
+  color: whitesmoke;
+}
+</style>
---
+++ vue-project/src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+import HelloWorld from '../components/HelloWorld.vue'
+
+</script>
+
+<template>
+  <main class="main">
+    <HelloWorld />
+  </main>
+</template>
+
+<style>
+.main {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  width: 100%;
+}
+</style>
